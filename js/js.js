9a10fb81dbf38d14ffc3bff14134e796f67fc941
begumbatan <143837896+begumbatan@users.noreply.github.com>
2025-05-02 14:11:04 -0600

    js

--- js/js.js
+++ js/js.js
@@ -1,36 +1,79 @@
-const noButton = document.getElementById('no');
-const yesButton = document.getElementById('yes');
-const stopButton = document.getElementById('stop');
+//const zerobutton = document.getElementById('zerobutton');
+//const onebutton = document.getElementById('onebutton');
+//const twobutton = document.getElementById('twobutton');
+//const threebutton = document.getElementById('threebutton');
+//const fourbutton = document.getElementById('fourbutton');
+//const fivebutton = document.getElementById('fivebutton');
+//const sixbutton = document.getElementById('sixbutton');
+//const sevenbutton = document.getElementById('sevenbutton');
+//const eightbutton = document.getElementById('eightbutton');
+//const ninebutton = document.getElementById('ninebutton');
 
-const digits = [];
-for (let i = 0; i < 10; i++) {
-  digits.push(document.getElementById('digit' + i));
+//zerobutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '3';
+//});
+//onebutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '4';
+//});
+//twobutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '5';
+//});
+//threebutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '6';
+//});
+//fourbutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '7';
+//});
+//fivebutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '8';
+//});
+//sixbutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '9';
+//});
+//sevenbutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '0';
+//});
+//eightbutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '1';
+//});
+//ninebutton.addEventListener('click', () => {
+//  document.getElementById('output').textContent = '2';
+//});
+const clearbutton = document.getElementById('clear');
+
+//phone number display
+const phonedisp =[]; 
+for (let i = 0; i < 10;i++) {
+  phonedisp.push(document.getElementById('d'+ i));
 }
-//https://stackoverflow.com/questions/19445408/how-to-restrict-user-to-type-10-digit-numbers-in-input-element
-//  digits.push(document.getElementById('digit' + i));
 
-let random;
-
-function rotate() {
-    random = setInterval(() => {
-    for (let i = 0; i < 10; i++) {
-      digits[i].textContent = Math.floor(Math.random() * 10);
+let currentIndex= 0;
+//add 3 to each digit
+function cipherdigit(n) {
+  return (n +3) % 10;
+}
+//gets button id and changes to the cipher one
+for (let i = 0; i <= 9;i++) {
+  const numberbuttons = [
+    "zerobutton", "onebutton","twobutton", "threebutton","fourbutton",
+    "fivebutton","sixbutton","sevenbutton","eightbutton", "ninebutton"
+  ][i];
+  
+const button =document.getElementById(numberbuttons);
+  button.addEventListener('click',() => {
+    if (currentIndex < 10){
+      const ciphered = cipherdigit(i);
+      phonedisp[currentIndex].textContent =ciphered;
+      currentIndex = currentIndex + 1;
     }
-  }, 100);
+  });
 }
-//https://stackoverflow.com/questions/72725263/what-will-math-floormath-random-10-1-do
-//      digits[i].textContent = Math.floor(Math.random() * 10);
 
-stopButton.addEventListener('click', () => {
-  clearInterval(random); 
+//clear button
+clearbutton.addEventListener('click', () => {
+  for (let i = 0; i < 10; i++){
+    phonedisp[i].textContent ='_';
+  }
+  currentIndex = 0;
 });
 
-yesButton.addEventListener('click', () => {
-  alert("Nice!");
-});
-
-noButton.addEventListener('click', () => {
-    rotate();
-});
-
-rotate();
